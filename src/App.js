0c30878395a8911f7a618446dcaf53dd42266952
Areeb0206 <masafvi48@gmail.com>
2021-06-26 11:20:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from "react";
-/*import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-  
-} from "react-router-dom";*/
+
 
 import './App.scss';
 
 import Home from './pages/Home/Home';
 
-/*import Login from './pages/Login/Login';
-
-import Account from './pages/Account/Account';
-
-import Contact from './pages/Contact/Contact';
-
-
-import Cart from './pages/Cart/Cart';
-*/
 
 
 
@@ -29,31 +15,6 @@ const App = () => {
   	<>
 
   	<Home />
-
- /*  <Router>
-
-    <Switch>
-
-         <Route exact path="/Login" component={Login} />
-
-         <Route exact path="/Account" component={Account} />
-
-         <Route exact path="/Contact" component={Contact} />
-
-         <Route exact path="/Cart" component={Cart} />
-
-
-
-
-           <Route exact path="/" component={Home} />
-
-
-        </Switch>
-
-
-
-
-   </Router>*/
 
    </>
   );
