f007ef85b6a259db99f3f93306962b6bf58f68a0
Areeb0206 <masafvi48@gmail.com>
2021-06-26 11:57:01 +0530

    first commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Nvbar from '../../Components/Navbar/Navbar';
-import './Home.scss';
+
 
 
 const Home = () => {
 
   return (
    <div className="section">
+    <Nvbar />
     <Nvbar />
    
    </div>
